e30f3c39c87c252ec17417a30022bec3695a95a7
Pétrus Blunk <tds742053@gmail.com>
2026-08-27 12:08:04 -0300

    Add files via upload

---
+++ mock.ts
@@ -0,0 +1,22 @@
+const data = [
+  {
+    id: "1",
+    title: "ROBÓTICA",
+    text: "A logística é o processo de planejar, executar e controlar o fluxo eficiente de materiais, produtos e informações desde o ponto de origem até o consumidor final.",
+    image: require("@/assets/images/card01.png"),
+  },
+  {
+    id: "2",
+    title: "SISTEMAS",
+    text: "A logística é o processo de planejar, executar e controlar o fluxo eficiente de materiais, produtos e informações desde o ponto de origem até o consumidor final.",
+    image: require("@/assets/images/card02.png"),
+  },
+  {
+    id: "3",
+    title: "LOGÍSTICA",
+    text: "A logística é o processo de planejar, executar e controlar o fluxo eficiente de materiais, produtos e informações desde o ponto de origem até o consumidor final.",
+    image: require("@/assets/images/card03.png"),
+  },
+];
+
+export default data;
